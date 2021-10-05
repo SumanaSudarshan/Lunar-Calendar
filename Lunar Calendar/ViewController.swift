775b0130a585a0afcdd5517093501c692733a8fb
Sumana Sudarshan <sudarshansumana@gmail.com>
2021-10-04 21:37:43 -0400

    Build 1

--- Lunar Calendar/ViewController.swift
+++ Lunar Calendar/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var  currentImageNumber = 0
+    let  date = Date()
+    var currentYear:Int = 0
+    var image = "zodiac"
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    
+    @IBOutlet weak var animalImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentYear = Calendar.current.component(.year, from: date)
+        currentImageNumber=1
+        animalImageView.image=UIImage.init(named: image+String(currentImageNumber))
+        yearLabel.text=String(currentYear)
+        //print(currentYear)
     }
-
-
+    
+    
+    @IBAction func leftButtonPressed(_ sender: Any) {
+        currentYear-=1
+ 
+            if currentImageNumber == 1{
+                currentImageNumber = 12
+            }
+            else{
+                currentImageNumber-=1
+            }
+        animalImageView.image=UIImage.init(named: image+String(currentImageNumber))
+        yearLabel.text=String(currentYear)
+        
+    }
+    
+    @IBAction func rightButtonPressed(_ sender: Any) {
+        currentYear+=1
+            if currentImageNumber == 12{
+                currentImageNumber = 1
+            }
+            else{
+                currentImageNumber+=1
+            }
+        animalImageView.image=UIImage.init(named: image+String(currentImageNumber))
+        yearLabel.text=String(currentYear)
+    }
 }
 
